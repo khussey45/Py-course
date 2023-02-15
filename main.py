46191fa3ca8dee83e1adb0102b196b01c72318ef
khussey45 <khussey45@gmail.com>
2023-02-15 16:50:37 -0500

    dsfadsaf

--- main.py
+++ main.py
@@ -148,11 +148,29 @@
 
 # Loop control statements = change a loops execution from its normal seqeunce 
 
-# break = used to terminate the loop entirely
-# continue = skips to the next iteration of the loop
-# pass = does nothing, acts as a placeholder
+# break     = used to terminate the loop entirely
+# continue  = skips to the next iteration of the loop
+# pass      = does nothing, acts as a placeholder
 
-while True:
-    name = input("Enter your name: ")
-    if name != "":
-        break
+# while True:
+#     name = input("Enter your name: ")
+#     if name != "":
+#         break
+
+# phone_number = "123-456-7890"
+
+# for i in phone_number: 
+#     if i == "-":
+#         continue
+#     print(i)
+
+# for i in range(1,21):
+#     if i == 13:
+#         pass
+#     else:
+#         print(i)
+
+
+# list = used to store multiple items in a single variable 
+
+food = "pizza"
