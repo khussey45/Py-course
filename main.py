40f748d01ec64588b66065dc898b2c6f0fc25436
khussey45 <khussey45@gmail.com>
2023-02-15 16:00:18 -0500

    ds

--- main.py
+++ main.py
@@ -67,16 +67,29 @@
 
 # slicing = create a substring by extracting elements from another string indexing[] or slice() [start:stop:step]
 
-name = "Bro Code"
+# name = "Bro Code"
 
-first_name = name[:3]
-last_name = name[4:]
-funky_name = name[0:8:3]
-reversed_name = name[::-1]
+# first_name = name[:3]
+# last_name = name[4:]
+# funky_name = name[0:8:3]
+# reversed_name = name[::-1]
 
-print(reversed_name) 
+# print(reversed_name) 
+
+# website  = "http://google.com"
+
+# slice  = slice(7,-4)
+
+# print(website[slice])
 
 
-
-
+age = int(input("How old are you?: "))
+if age == 100:
+    print("You are too old")
+elif age >= 18:
+    print("you are an adult")
+elif age < 0:
+    print("You haven't been born yet! ")
+else:
+    print("You are a child!")
 
