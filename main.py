e808d6ec2cf25c4498de71f1abb0daa7baf2750c
khussey45 <khussey45@gmail.com>
2023-02-15 11:22:18 -0500

    e

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("I love pizza")
-print("it's really good")
+# first_name = "Kieren "
+# last_name = "Hussey"
+# full_name = first_name + last_name
+
+# print("Hello " + full_name)
+
+age = 21
+age += 1
+print(type(age)) 
