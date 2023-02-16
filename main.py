a356ef03b57d11801f2894df007e5e6436b06543
khussey45 <khussey45@gmail.com>
2023-02-16 16:00:14 -0500

    d

--- main.py
+++ main.py
@@ -351,5 +351,24 @@
 # print(cards)
 
 
+# exception = events detected during execution that interrupt the flow of a program
 
+# try:
+#     numerator = int(input("Enter a number to divide: "))
+#     denominator = int(input('Enter a number to divide by: '))
+#     result = numerator / denominator
+# except ZeroDivisionError as e:
+#     print("You can't divide by 0 idiot")
+#     print(e)
+# except ValueError as e:
+#     print("Enter only numbers plz")
+#     print(e)
+# except Exception as e:
+#     print("something went wrong")
+#     print(e)
+# else:
+#     print(result)
+# finally:
+#     print("This will always execute")
 
+    
