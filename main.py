a913ae4d2c08b3f8022c7d90a0a12edb27dd90ff
khussey45 <khussey45@gmail.com>
2023-02-16 16:08:30 -0500

    d

--- main.py
+++ main.py
@@ -372,3 +372,15 @@
 #     print("This will always execute")
 
     
+# import os
+
+# path = "C:\\Users\\kiere\\OneDrive\\Desktop\\Py-code"
+
+# if os.path.exists(path):
+#     print("That location exists!")
+#     if os.path.isfile(path):
+#         print("Thats a file")
+#     elif os.path.isdir(path):
+#         print("That is a directory!")
+# else:
+#     print("That location doesn't exist!")
