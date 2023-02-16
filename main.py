d30df730364d3348d69ef3b7ae515a29a580a100
khussey45 <khussey45@gmail.com>
2023-02-16 11:00:43 -0500

    d

--- main.py
+++ main.py
@@ -245,3 +245,15 @@
 
 # index oeprator [] = gives access to as sequence's elemt (str,list,tuples)
 
+# name = "bro Code!"
+
+# # if (name[0].islower()):
+# #     name = name.capitalize()
+
+# first_name = name[:3].upper()
+# last_name = name[4:].lower()
+# last_character = name[-1]
+
+# print(first_name)
+# print(last_name)
+# print(last_character)
