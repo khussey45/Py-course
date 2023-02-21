2cd0e7a0e1b85ef9335b26c8338df85fa3697bc4
khussey45 <khussey45@gmail.com>
2023-02-21 10:43:00 -0500

    f

--- main.py
+++ main.py
@@ -463,29 +463,29 @@
 
 # QUIZ GAME
 # -------------------------
-# def new_game():
-#     pass
-# # -------------------------
-# def check_answer():
-#     pass
-# # -------------------------
-# def display_score():
-#     pass
-# # -------------------------
-# def play_again():
-#     pass
-# # ------------------------- 
+def new_game():
+    pass
+# -------------------------
+def check_answer():
+    pass
+# -------------------------
+def display_score():
+    pass
+# -------------------------
+def play_again():
+    pass
+# ------------------------- 
 
-# questions = {
-#     "Who created Python?: ": "A", 
-#     "what year was Python created?: ": "B",
-#     "Python is tributed to which comedy group?: ": "C",
-#     "Is the Earth round?: ": "A"
-# }
+questions = {
+    "Who created Python?: ": "A", 
+    "what year was Python created?: ": "B",
+    "Python is tributed to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"
+}
 
-# options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
-#           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
-#           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
-#           ["A. True","B. False", "C. sometimes", "D. What's Earth?"]] 
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+          ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+          ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+          ["A. True","B. False", "C. sometimes", "D. What's Earth?"]] 
 
-# new_game()
+new_game()
